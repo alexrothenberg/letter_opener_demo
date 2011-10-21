c85f3da48ab4d71013cb026a8ce30018c7066022
Alex Rothenberg <alex@alexrothenberg.com>
2011-10-21 11:25:22 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LetterOpenerDemo::Application.config.secret_token = 'e9aa02b00fc2b162dac002fc667f66c9c93a9ddf14c6d0937505225ac77d16cb57dad7a5092bee908f8b68db84ab49b49b4cf4035c73076651d75455299495d7'
